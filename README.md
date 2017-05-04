d01008d3e203c6c47fc4d47ac974fb0540e954e0
Jason <jason@Jasons-MacBook-Pro.local>
2017-05-04 01:31:23 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# actionfps-log-stream
-Streams the logs served by ActionFPS using EventSource.
+# ActionFPS Log Stream
+Streams the logs served by ActionFPS using EventSource to a file.
+
+## Running
+*   Create the file `config.js` in the root folder
+
+    Example `config.js` file:
+    ```javascript
+    module.exports = {
+      logFilePath: 'test.log',
+      url: 'https://actionfps.com/logs',
+      authorization: '', // Can be left blank if you don't have a JWT
+      lastLineDate: '2017-05-04T00:00:00Z',
+    };
+    ```
+*   Run `npm install && node index`
---
+++ index.js
@@ -0,0 +1,54 @@
+const EventSource = require('eventsource');
+const childProcess = require('child_process');
+const fs = require('fs');
+const config = require('./config');
+
+// Retrieves the last date in the log file to resume streaming.
+// Returns config.headers.lastLineDate if the log file isn't found.
+function getLastEventDate() {
+  try {
+    const lastLineBuf = childProcess.execSync(`tail -1 ${config.logFilePath}`);
+    const lastLine = lastLineBuf.toString('utf8');
+    const lastEventDate = lastLine.substring(0, 20);
+    if (lastEventDate === '') {
+      throw new Error({ message: 'No date found in file.' });
+    } else {
+      console.log(`Resuming streaming from ${lastEventDate}`);
+      return lastEventDate;
+    }
+  } catch (e) {
+    console.log(`File not found... starting from ${config.lastLineDate}`);
+    return config.lastLineDate;
+  }
+}
+
+// Open file stream
+const writeStream = fs.createWriteStream(config.logFilePath, {
+  flags: 'a',
+  encoding: 'utf8',
+});
+
+// Open Event Source connection
+const eventSource = new EventSource(config.url, {
+  headers: {
+    Authorization: config.authorization,
+    'Last-Event-ID': getLastEventDate(),
+  },
+});
+
+eventSource.addEventListener('message', (e) => {
+  if (e.data !== '') {
+    writeStream.write(`${e.data}\n`);
+  }
+}, false);
+
+eventSource.addEventListener('open', () => {
+  console.log(`Connected to ${config.url}`);
+}, false);
+
+eventSource.addEventListener('error', (e) => {
+  if (e.readyState === EventSource.CLOSED) {
+    writeStream.end();
+    console.log('Connection was closed');
+  }
+}, false);
